056c5c8db8628ebd032093890b15a78eb0fcc957
Rahul Janardhanan <rahul25jana@gmail.com>
2018-07-17 17:02:15 -0500

    Ship Control

---
+++ Assets/shipControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipControl : MonoBehaviour {
+
+    public GameObject Ship;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+    {
+		
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("rot left");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("rot right");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("thurst");
+        }
+   
+	}
+}
