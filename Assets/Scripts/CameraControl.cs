7797f2a03529ffca3d33aaf75ee3906f1bb74222
Rahul Janardhanan <rahul25jana@gmail.com>
2018-07-31 13:32:12 -0500

    Camera Control !

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    
+    public GameObject PlayerShip;
+    private Vector3 CamPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+        CamPos = transform.position - PlayerShip.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = PlayerShip.transform.position + CamPos;
+        //this.transform.position = PlayerShip.transform.position + new Vector3(CamPos.x, CamPos.y, 0);
+	}
+}
